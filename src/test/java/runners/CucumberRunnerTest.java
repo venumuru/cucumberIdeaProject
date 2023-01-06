46cf6b6151fdbf5bf7b64a0bf7996359427d51a6
venugopalmurugan <61824073+venugopalmurugan@users.noreply.github.com>
2023-01-06 19:54:36 +0530

    initial changes

---
+++ src/test/java/runners/CucumberRunnerTest.java
@@ -0,0 +1,13 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+
+//@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-report.html"},features="src/test/resources/feature",glue={"StepDefinitions"})
+public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
+}
