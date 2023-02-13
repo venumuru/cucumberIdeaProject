685f5c62550523dd3c3d933361ee1a1e0796643b
venugopalmurugan <61824073+venugopalmurugan@users.noreply.github.com>
2023-02-13 13:30:08 +0530

    latest changes

--- src/test/java/runners/CucumberRunnerTest.java
+++ src/test/java/runners/CucumberRunnerTest.java
@@ -3,6 +3,11 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber-report.html"},features="src/test/resources/feature",glue={"StepDefinitions"})
+@CucumberOptions(plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+        ,features="src/test/resources/feature"
+        ,glue={"StepDefinitions"}
+        ,monochrome=true
+        ,publish = true
+)
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
 }
