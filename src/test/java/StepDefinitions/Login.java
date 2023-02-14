6f02e5e4dc5a36d6785c3c2818286d00dd911056
venugopalmurugan <61824073+venugopalmurugan@users.noreply.github.com>
2023-02-14 14:51:32 +0530

    Update Login.java

--- src/test/java/StepDefinitions/Login.java
+++ src/test/java/StepDefinitions/Login.java
@@ -19,7 +19,7 @@ public class Login {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://www.google.com");
