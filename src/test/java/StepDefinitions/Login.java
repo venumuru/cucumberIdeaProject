a9f6a342dfa691ba150a567f884a4c87f304ff6c
venugopalmurugan <61824073+venugopalmurugan@users.noreply.github.com>
2023-02-14 14:33:26 +0530

    Latest changes

--- src/test/java/StepDefinitions/Login.java
+++ src/test/java/StepDefinitions/Login.java
@@ -3,11 +3,27 @@ package StepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class Login {
-
+    public WebDriver driver;
     @Given("user is on the application login page")
     public void loginPage(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        driver.close();
         System.out.println("user is on application login page");
     }
 
